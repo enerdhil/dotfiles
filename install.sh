20066d768655cdbda6bb327c9be892de75ee5aeb
Enerdhil <hezardj@gmail.com>
2017-03-06 22:32:31 +0100

    Begun install.sh

---
+++ install.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+function ask() {
+	while true; do
+		read -e -n 1 -p "Do you want to install $1 : [Y]es/[N]o : " ANSWER
+		echo
+		case $ANSWER in
+			[Yy]* ) return 0;;
+			[Nn]* ) return 1;;
+		esac
+	done
+}
+
+if [[ $1 == "-f" ]]; then
+	FORCE=1
+else
+	FORCE=0
+fi
+
+# Git config file
+if [ $FORCE -eq 1 ] || ask "git config"; then
+	echo "Installing git config"
+	cp -v irbc $HOME/.irbc
+	echo
+else
+	echo "Skipping git config install"
+fi
+
+# ZSH config files
+if [ $FORCE == 1 ] || ask "ZSH config"; then
+	echo "Installing git config"
+	if command -v wget >/dev/null 2>&1; then
+		sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+	elif command -v curl >/dev/null 2>&1; then
+		sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+	else
+		echo "Don't have curl or wget, install oh-my-zsh manually."
+		exit 1
+	fi
+
+	echo
+else
+	echo "Skipping zsh config install"
+fi
+# Hosts file
+if [ $FORCE -eq 1 ] || ask "hosts file"; then
+	echo "Installing hosts file for ssh completion"
+	openssl enc -aes-256-cbc -d -in hosts.dat > $HOME/.hosts
+	echo
+else
+	echo "Skipping hosts file install"
+fi
+
+# IRB config files
+if [ $FORCE == 1 ] || ask "IRB config"; then
+	echo "Installing irb config"
+	cp -v irbc $HOME/.irbc
+	cp -v irbc_rails $HOME/.irbc_rails
+	echo
+else
+	echo "Skipping IRB config install"
+fi
+
